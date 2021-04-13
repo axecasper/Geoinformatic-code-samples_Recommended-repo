05474c8b84d0b60cb9f09e58a972f23e56e2e8f6
axecasper <46599886+axecasper@users.noreply.github.com>
2021-04-13 11:57:52 +0300

    Add files via upload

---
+++ Basic Coding and Statistic/Moran-I/Berk_Kivilcim_Moran.m
@@ -0,0 +1,242 @@
+Adana=01;
+Adiyaman=02;
+Afyonkarahisar=03;
+Agri=04;
+Amasya=05;
+Ankara=06;
+Antalya=07;
+Artvin=08;
+Aydin=09;
+Balikesir=10;
+Bilecik=11;
+Bingol=12;
+Bitlis=13;
+Bolu=14;
+Burdur=15;
+Bursa=16;
+canakkale=17;
+cankiri=18;
+corum=19;
+Denizli=20;
+Diyarbakir=21;
+Edirne=22;
+Elazig=23;
+Erzincan=24;
+Erzurum=25;
+Eskisehir=26;
+Gaziantep=27;
+Giresun=28;
+Gumushane=29;
+Hakkari=30;
+Hatay=31;
+Isparta=32;
+Mersin=33;
+Istanbul=34;
+Izmir=35;
+Kars=36;
+Kastamonu=37;
+Kayseri=38;
+Kirklareli=39;
+Kirsehir=40;
+Kocaeli=41;
+Konya=42;
+Kutahya=43;
+Malatya=44;
+Manisa=45;
+Kahramanmaras=46;
+Mardin=47;
+Mugla=48;
+Mus=49;
+Nevsehir=50;
+Nigde=51;
+Ordu=52;
+Rize=53;
+Sakarya=54;
+Samsun=55;
+Siirt=56;
+Sinop=57;
+Sivas=58;
+Tekirdag=59;
+Tokat=60;
+Trabzon=61;
+Tunceli=62;
+sanliurfa=63;
+Usak=64;
+Van=65;
+Yozgat=66;
+Zonguldak=67;
+Aksaray=68;
+Bayburt=69;
+Karaman=70;
+Kirikkale=71;
+Batman=72;
+sirnak=73;
+Bartin=74;
+Ardahan=75;
+Igdir=76;
+Yalova=77;
+Karabuk=78;
+Kilis=79;
+Osmaniye=80;
+Duzce=81;
+
+
+
+komsu2=[Tekirdag Edirne Balikesir 0 0 0 0 0 0 ;
+    Karabuk Kastamonu Bolu Ankara Kirikkale corum 0 0 0;
+    Amasya Kastamonu Samsun Sinop cankiri Kirikkale Yozgat 0 0;
+    Kahramanmaras Kayseri Hatay Osmaniye Mersin Nigde 0 0 0;
+    Diyarbakir Kahramanmaras Malatya Gaziantep sanliurfa 0 0 0 0;
+    Konya Kutahya Eskisehir Isparta Denizli Burdur Usak 0 0;
+    Igdir Erzurum Kars Bitlis Mus Van 0 0 0;
+    Konya Nevsehir Nigde Ankara Kirsehir 0 0 0 0;
+    Tokat Samsun corum Yozgat 0 0 0 0 0;
+    Aksaray Konya Bolu cankiri Kirikkale Kirsehir Eskisehir 0 0;
+    Mersin Karaman Konya Isparta Burdur Mugla 0 0 0 ;
+    Erzurum Kars Artvin 0 0 0 0 0 0;
+    Erzurum Ardahan Rize 0 0 0 0 0 0;
+    Denizli Mugla Manisa Izmir 0 0 0 0 0;
+    Kutahya Bursa Manisa Izmir canakkale 0 0 0 0;
+    Karabuk Kastamonu Zonguldak 0 0 0 0 0 0;
+    Mardin Bitlis Mus Siirt Diyarbakir 0 0 0 0;
+    Erzurum Rize Erzincan Gumushane Trabzon 0 0 0 0;
+    Sakarya Bolu Kutahya Eskisehir Bursa 0 0 0 0;
+    Erzurum Mus Elazig Erzincan Diyarbakir Tunceli 0 0 0;
+    Agri Batman Mus Siirt Van 0 0 0 0;
+    Karabuk Zonguldak Sakarya Duzce Ankara cankiri Eskisehir Bilecik 0;
+    Afyonkarahisar Isparta Denizli Antalya Mugla 0 0 0 0;
+    Sakarya Kutahya Bilecik Kocaeli Balikesir Yalova 0 0 0;
+    Zonguldak Sakarya Bolu 0 0 0 0 0 0;
+    Afyonkarahisar Burdur Usak Mugla Aydin Manisa 0 0 0;
+    Mardin Batman Mus Elazig Bingol Malatya Adiyaman sanliurfa 0;
+    Tekirdag Kirklareli canakkale 0 0 0 0 0 0;
+    Erzincan Bingol Diyarbakir Tunceli Malatya 0 0 0 0;
+    Erzurum Elazig Bayburt Bingol Gumushane Giresun Tunceli Sivas Malatya;
+    Agri Kars Artvin Ardahan Rize Mus Erzincan Bayburt Bingol;
+    Konya Bolu Ankara Afyonkarahisar Kutahya Bilecik 0 0 0;
+    Erzincan Bayburt Giresun Trabzon 0 0 0 0 0;
+    Kahramanmaras Adiyaman Kilis Hatay Osmaniye sanliurfa 0 0 0;
+    Erzincan Gumushane Trabzon Ordu Sivas 0 0 0 0;
+    sirnak Van 0 0 0 0 0 0 0;
+    Gaziantep Adana Osmaniye 0 0 0 0 0 0;
+    Agri Kars 0 0 0 0 0 0 0;
+    Konya Afyonkarahisar Burdur Antalya 0 0 0 0 0;
+    Kocaeli Tekirdag 0 0 0 0 0 0 0;
+    Aydin Manisa Balikesir 0 0 0 0 0 0;
+    Sivas Kayseri Malatya Adiyaman Gaziantep Adana Osmaniye 0 0;  %kastamonu
+    Afyonkarahisar Eskisehir Bilecik Bursa Usak Manisa Balikesir 0 0;
+    Bartin Kastamonu Zonguldak Bolu cankiri 0 0 0 0; %
+    Mersin Konya Antalya 0 0 0 0 0 0;
+    Agri Igdir Erzurum Ardahan 0 0 0 0 0;
+    Karabuk Bartin Sinop cankiri corum 0 0 0 0;
+    Sivas Kahramanmaras Adana Nevsehir Nigde Yozgat 0 0 0;
+    Gaziantep 0 0 0 0 0 0 0 0;
+    Ankara cankiri Kirsehir corum Yozgat 0 0 0 0;
+    Tekirdag Edirne 0 0 0 0 0 0 0;
+    Aksaray Nevsehir Ankara Kirikkale Yozgat 0 0 0 0;
+    Sakarya Bursa Istanbul Yalova 0 0 0 0 0;
+    Mersin Karaman Aksaray Nigde Ankara Afyonkarahisar Eskisehir Isparta Antalya;
+    Elazig Erzincan Diyarbakir Sivas Kahramanmaras Adiyaman 0 0 0;
+    Kutahya Denizli Usak Aydin Izmir Balikesir 0 0 0;
+    Batman Siirt sirnak Diyarbakir sanliurfa 0 0 0 0;
+    Adana Karaman Konya Nigde Antalya 0 0 0 0;
+    Denizli Burdur Antalya Aydin 0 0 0 0 0;
+    Agri Erzurum Batman Bitlis Bingol Diyarbakir 0 0 0;
+    Kayseri Aksaray Nigde Kirsehir Yozgat 0 0 0 0;
+    Kayseri Adana Mersin Aksaray Konya Nevsehir 0 0 0;
+    Giresun Sivas Tokat Samsun 0 0 0 0 0;
+    Kahramanmaras Gaziantep Adana Hatay 0 0 0 0 0;
+    Erzurum Artvin Bayburt Trabzon 0 0 0 0 0;
+    Duzce Bolu Bilecik Bursa Kocaeli 0 0 0 0;
+    Amasya Ordu Tokat Sinop corum 0 0 0 0;
+    Mardin Diyarbakir Adiyaman Gaziantep 0 0 0 0 0;
+    Mardin Batman Bitlis sirnak Van 0 0 0 0;
+    Kastamonu Samsun corum 0 0 0 0 0 0;
+    Hakkari Mardin Siirt Van 0 0 0 0 0;
+    Erzincan Giresun Ordu Tokat Kahramanmaras Kayseri Malatya Yozgat 0;
+    Istanbul Edirne Kirklareli canakkale 0 0 0 0 0;
+    Amasya Ordu Sivas Samsun Yozgat 0 0 0 0;
+    Rize Bayburt Gumushane Giresun 0 0 0 0 0;
+    Elazig Erzincan Bingol 0 0 0 0 0 0;
+    Afyonkarahisar Kutahya Denizli Manisa 0 0 0 0 0;
+    Agri Hakkari Bitlis Siirt sirnak 0 0 0 0;
+    Bursa Kocaeli 0 0 0 0 0 0 0;
+    Amasya Sivas Tokat Kayseri Nevsehir Kirikkale Kirsehir corum 0;
+    Karabuk Bartin Duzce Bolu 0 0 0 0 0];
+
+plaka=[17 18 19 1 2 3 4 68 5 6 7 75 8 9 10 74 72 69 11 12 13 14 15 16 81 20 21 22 23 24 25 26 29 27 28 30 31 76 32 34 35 46 43 78 70 36 37 38 79 71 39 40 41 42 44 45 47 33 48 49 50 51 52 80 53 54 55 63 56 57 73 58 59 60 61 62 64 65 77 66 67];
+
+
+%2017 verisi
+density=[53.3994764924998 24.8429906542056 41.3087867417136 159.286740927057 87.455708801365 49.9995109682828 46.7554489973845 53.1577278731836 57.9768014059754 222.055625790139 114.095256478309 20.0528707145808 22.5523279489616 137.668959368233 84.2593188334849 93.0658653846154 125.617514488088 21.5076223589195 51.5325430032543 33.1217739003999 48.6360917248255 36.4403846153846 38.7103801169591 281.78881212819 147.101675107129 87.1309442353746 112.8902244654 66.9830424761278 69.0326434062685 19.9252087098718 30.0310389764246 62.1745412512643 26.4366941121641 294.106907171139 64.0212236533958 38.4121743975484 270.285861358957 54.2851170568562 52.4202513291445 2892.4617013087 356.283466533467 78.601909940053 47.7795775235869 59.4920905329764 27.888298473714 28.4046608077417 28.3108796472288 80.7793228891627 95.4614845938375 61.4797088663432 56.7139216310927 36.9220717884131 521.392580287929 56.0838885601832 66.8033288043478 107.898671350031 91.9508289802407 115.849596383597 73.0488677923897 50.1977912892418 54.3530395984384 47.9770130576714 124.72127016129 168.925736235595 84.4061703212647 204.674245556015 144.554662556424 105.822168931521 59.2716974237164 35.8126726519337 70.3629753914989 21.7626186556447 159.268651987961 60.4625426792529 168.594768439108 11.1003767491927 68.3338326156151 57.3548370381885 296.579693034239 29.7505685048323 180.657384987893]
+%2007-2008 verisi
+%density=[-2.81201843904968  11.8880022163794  -8.00536015282174  9.82096176435956  3.94750086389067  -6.01458490614747  2.44765464480452  12.1868139114547  -16.2813499913286  18.2316008686414  38.3649392700674  -4.25848449634978  -9.01176185689376  19.3776321714484  10.6405509161627  17.6168300714194  27.4481540121015  -12.2667084095307  -53.4608052614972  17.8831224541684  -3.02085042554939  -5.6925903634614  -15.017790638744  27.5236515291662  16.2073925449337  11.5180153454354  21.7247687517992  -4.59610520034639  11.5796374804665  -13.640550691284  -12.7881079205385  23.0340719663871  4.13438069840657  32.9134072431703  10.1541376059474  48.0075709046911  19.3346931486711  11.8014657794952  -29.9354645309764  9.76051435208286  15.0294833478649  25.1370189980464  -31.3577511608722  -10.1907665978739  17.9577551344939  -0.246663240259298  0.16093453051562  16.427876198855  21.1661371411953  -3.24899000681617  10.9998453174925  -1.95108885522519  35.8145619407889  5.49053925157366  16.1042527080109  -2.40454931969031  6.57413951626715  4.35782843588593  32.4480501379133  -2.9636310339132  5.84240042499856  20.2059031927419  5.39352399391488  24.0063358576475  9.93617986221959  19.0576376760071  4.08720468327278  33.0153773657555  28.0429031779931  11.9188892032998  31.4380288133011  -10.9010943351086  56.5477536350548  -6.45550012869848  11.2961415352891  28.4759753682549  -0.0119719974979851  24.8979615953427  82.6167834741631  -16.2263772628584  5.28080861813052];
+[rr2 cc2]=size(plaka)
+[r2 c2]=size(komsu2)
+
+%making a matrix which shows how many neighbor has the city
+for i=1:r2
+    komsuluk=0;
+    for j=1:c2
+        if komsu2(i,j)~=0
+            komsuluk=komsuluk+1;
+            yeni(1,i)=komsuluk;
+        end
+    end
+end
+disp(yeni)
+
+%making 81x81 weight matrix with 1.
+bos2=zeros(81,81);
+for i=1:r2
+    for j=1:c2
+        for k=1:cc2
+            if komsu2(i,j)==plaka(1,k)& plaka(1,k)~=0
+                bos2(i,k)=1;
+            
+            end
+        end
+    end
+end  
+
+%dividing the 1 values with how many 1 in this row.
+for i=1:81
+    for j=1:81
+        bos2(i,j)=bos2(i,j)/yeni(1,i);
+    end   
+end
+
+%totals of the each values of weight matrix
+W=0;
+for i=1:81
+    for j=1:81
+        W=W+bos2(i,j);
+    end
+end   
+disp(W)
+
+[rrr ccc]=size(density);
+
+A=0;
+%mean of the density
+for i=1:ccc
+    A=A+density(1,i);
+end
+ortalama=A/ccc;
+ust=0;
+alt=0;
+
+for i=1:81
+    for j=1:81
+        ust=ust+(bos2(i,j)*((density(1,i)-ortalama)*(density(1,j)-ortalama)));
+    end
+end  
+
+for i=1:81
+    alt=alt+((density(1,i)-ortalama)^2);
+end
+N=ccc;
+format long g
+disp('Result is')
+disp(((ust)/(alt))*N/W)
+
