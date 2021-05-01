36f6cb13c4b3457eb80f313816db5997d3421c2f
axecasper <46599886+axecasper@users.noreply.github.com>
2021-05-01 19:25:31 +0300

    Add files via upload

---
+++ Basic Coding and Statistic/python_qgis_plugin_development/i18n/saveattributes.pro
@@ -0,0 +1,4 @@
+SOURCES         = ..\saveattributes.py
+FORMS           = ..\*.ui
+TRANSLATIONS    = saveattributes_tr.ts \
+CODECFORTR      = ISO-8859-5
---
+++ Basic Coding and Statistic/python_qgis_plugin_development/i18n/saveattributes_tr.ts
@@ -0,0 +1,48 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="tr_TR">
+<context>
+    <name>SaveAttributesDialogBase</name>
+    <message>
+        <location filename="../save_attributes_dialog_base.ui" line="20"/>
+        <source>Save Attributes</source>
+        <translation>Nitelikleri Kaydet</translation>
+    </message>
+    <message>
+        <location filename="../save_attributes_dialog_base.ui" line="28"/>
+        <location filename="../save_attributes_dialog_base.ui" line="83"/>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+    <message>
+        <location filename="../save_attributes_dialog_base.ui" line="52"/>
+        <source>Input file</source>
+        <translation>Girdi Dosyası</translation>
+    </message>
+    <message>
+        <location filename="../save_attributes_dialog_base.ui" line="59"/>
+        <source>Select Layer</source>
+        <translation>Katman Seç</translation>
+    </message>
+    <message>
+        <location filename="../save_attributes_dialog_base.ui" line="69"/>
+        <source>Run</source>
+        <translation>Çalıştır</translation>
+    </message>
+    <message>
+        <location filename="../save_attributes_dialog_base.ui" line="76"/>
+        <source>Output File</source>
+        <translation>Çıktı Dosyası</translation>
+    </message>
+    <message>
+        <location filename="../save_attributes_dialog_base.ui" line="112"/>
+        <source>ID Field</source>
+        <translation>ID Alanı</translation>
+    </message>
+    <message>
+        <location filename="../save_attributes_dialog_base.ui" line="122"/>
+        <source>No ID</source>
+        <translation>ID Yok</translation>
+    </message>
+</context>
+</TS>
